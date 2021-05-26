387a34feabd76e8fe2232cbe87fbbe24f980bacd
Antonio Sousa <developerantoniosousa@gmail.com>
2021-05-26 17:43:46 -0300

    setting up a realm path

--- src/services/realm.js
+++ src/services/realm.js
@@ -4,6 +4,7 @@ import RepositorySchema from 'schemas/RepositorySchema';
 
 export default function getRealm() {
     return Realm.open({
+        path: 'database',
         schema: [RepositorySchema]
     });
 }
