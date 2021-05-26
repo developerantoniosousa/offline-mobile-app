b35fbd15dd477deb6729cf67cfc13435e0faa096
Antonio Sousa <developerantoniosousa@gmail.com>
2021-05-26 14:01:53 -0300

    create Main screen partially
    already added title, input for user type the repo name and button to add new repos

---
+++ src/scenes/Main/styles.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components/native';
+import LinearGradient from 'react-native-linear-gradient';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+
+export const Container = styled(LinearGradient).attrs({
+    colors: ['#7159c1', '#9849c1'],
+    start: {x: 0, y: 0},
+    end: { x: 1, y: 1 }
+})`
+    flex: 1;
+    padding-top: ${30 + getStatusBarHeight(true)}px;
+`;
+
+export const Title = styled.Text`
+    font-size: 32px;
+    color: #FFF;
+    font-weight: bold;
+    padding: 0 20px;
+`;
+
+export const Form = styled.View`
+    flex-direction: row;
+    margin-top: 10px;
+    padding: 0 20px;
+`;
+
+export const Input = styled.TextInput.attrs({
+    placeholderTextColor: '#999'
+})`
+    flex: 1;
+    padding: 12px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+    color: #333;
+    background: #FFF;
+`;
+
+export const Submit = styled.TouchableOpacity.attrs({
+    activeOpacity: 0.7
+})`
+    background: #6bd4c1;
+    margin-left: 10px;
+    justify-content: center;
+    border-radius: 4px;
+    padding: 0 14px;
+`;
